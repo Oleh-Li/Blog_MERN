453320e031de1147ee730443a428ba04d7c1c0b3
Oleh <88395278+Oleh-Li@users.noreply.github.com>
2024-06-04 00:35:35 +0300

    added auth/login

--- index.js
+++ index.js
@@ -24,6 +24,46 @@ app.get("/", (req, res) => {
     res.send("Hello World!")
 })
 
+app.post("/auth/login", async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email })
+
+        if (!user) {
+            return res.status(404).json({
+                message: "Wrong email or password"
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+        if (!isValidPass) {
+            return res.status(404).json({
+                message: "Wrong email or password"
+            })
+        }
+
+        const token = jwt.sign({
+            _id: user._id
+        },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "30d"
+            }
+        )
+
+        // kind of destruction - take psswordHash but other values collect in userData
+        const { passwordHash, ...userData } = user._doc
+
+        res.json({ ...userData, token })
+
+    } catch (err) {
+        console.log("err=>", err)
+        res.status(500).json({
+            message: "authorization failed",
+        })
+    }
+})
+
 app.post("/auth/register", registerValidation, async (req, res) => {
     try {
         const { email, fullName, avatarUrl } = req.body
@@ -33,16 +73,31 @@ app.post("/auth/register", registerValidation, async (req, res) => {
         }
         const password = req.body.password
         const salt = await bcrypt.genSalt(10)
-        const passwordHash = await bcrypt.hash(password, salt)
+        const hash = await bcrypt.hash(password, salt)
 
         const doc = new UserModel({
             email,
             fullName,
             avatarUrl,
-            passwordHash
+            passwordHash: hash
         })
+
         const user = await doc.save()
-        res.json(user)
+
+        const token = jwt.sign({
+            _id: user._id
+        },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "30d"
+            }
+        )
+
+        // kind of destruction - take psswordHash but other values collect in userData
+        const { passwordHash, ...userData } = user._doc
+
+        res.json({ ...userData, token })
+
     } catch (err) {
         console.log("err=>", err)
         res.status(500).json({
