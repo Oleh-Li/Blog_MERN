e6e7b89e3142dd2c21adc99500cc46d35e2f2366
Oleh <88395278+Oleh-Li@users.noreply.github.com>
2024-06-14 19:17:31 +0300

    added multer and ability upload images

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import multer from "multer"
 
 import { loginValidation, postCreateValidation, registerValidation } from "./validations.js"
 
@@ -19,7 +20,19 @@ mongoose
 
 const app = express()
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage })
+
 app.use(express.json()) //without this will be undefined in req.body
+app.use('/uploads', express.static('uploads'))
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
@@ -29,11 +42,17 @@ app.post("/auth/login", loginValidation, UserController.login)
 app.post("/auth/register", registerValidation, UserController.register)
 app.get("/auth/me", checkAuth, UserController.getMe)
 
+app.post("/upload", checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    })
+})
+
 app.get("/posts", PostController.getAll)
 app.get("/posts/:id", PostController.getOne)
-app.post("/posts", postCreateValidation, checkAuth, PostController.create)
+app.post("/posts", checkAuth, postCreateValidation, PostController.create)
 app.delete("/posts/:id", checkAuth, PostController.remove)
-app.patch("/posts/:id", checkAuth, PostController.update)
+app.patch("/posts/:id", checkAuth, postCreateValidation, PostController.update)
 
 app.listen(4444, (err) => {
     if (err) {
